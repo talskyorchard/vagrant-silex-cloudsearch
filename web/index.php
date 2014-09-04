2d858894f800406650f0d9bac357309ced7cb4ab
Daniel Talsky <talskyorchard@github.com>
2014-09-04 14:01:31 -0400

    added routing

--- web/index.php
+++ web/index.php
@@ -5,12 +5,43 @@ $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
-$app->get('/', function() {
-    return "Hello World!";
+
+
+/*
+ * Basic Index
+ */
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('index.twig', array(
+        'name' => $name,
+    ));
 });
+
+/*
+ * Silex's original example
+ */
 $app->get('/hello/{name}', function ($name) use($app) {
     return $app['twig']->render('hello.twig', array(
         'name' => $name,
     ));
 });
+
+
+/*
+ * Embryonic search term JSON response
+ */
+$app->get('/search/{term}', function ($term) use($app) {
+	$fakeresults = array(
+			'term' => array(
+				'original_request' => $term,
+				'fake_response' => 'THIS IS A FAKE RESPONSE'
+			)
+	);
+	return $app->json($fakeresults);
+});
+
+
+
+
+
+
 $app->run();
