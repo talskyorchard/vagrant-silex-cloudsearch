7c60d2a6a180a1e45a6548f6e38086a499ff2d4a
Daniel Talsky <talskyorchard@github.com>
2014-09-04 16:55:45 -0400

    Created an embryonic config and got search working

--- web/index.php
+++ web/index.php
@@ -1,10 +1,20 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', True);
+
+
 require_once __DIR__.'/../vendor/autoload.php'; 
+
+use Aws\CloudSearch\CloudSearchClient;
 
 $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+
+$app['debug'] = true;
+$app['config'] = parse_ini_file(__DIR__.'/../config/config.ini');
 
 
 /*
@@ -12,7 +22,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
  */
 $app->get('/', function() use ($app) {
     return $app['twig']->render('index.twig', array(
-        'name' => $name,
+        'name' => 'test',
     ));
 });
 
@@ -30,18 +40,16 @@ $app->get('/hello/{name}', function ($name) use($app) {
  * Embryonic search term JSON response
  */
 $app->get('/search/{term}', function ($term) use($app) {
-	$fakeresults = array(
-			'term' => array(
-				'original_request' => $term,
-				'fake_response' => 'THIS IS A FAKE RESPONSE'
-			)
-	);
-	return $app->json($fakeresults);
+	
+	//The SDK will use the $_SERVER superglobal and/or getenv() function to look for the 
+	//AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables. 
+	$_SERVER['AWS_ACCESS_KEY_ID'] = $app['config']['AWS_ACCESS_KEY_ID'];
+	$_SERVER['AWS_SECRET_ACCESS_KEY'] = $app['config']['AWS_SECRET_ACCESS_KEY'];	
+	$configClient = CloudSearchClient::factory(array('region'  =>  $app['config']['AWS_REGION']));
+	$domainClient = $configClient->getDomainClient( $app['config']['AWS_URL']);
+	$result = $domainClient->search(array('query' => $term));
+	$hits = $result->getPath('hits');
+	return $app->json($hits);
 });
-
-
-
-
-
 
 $app->run();
