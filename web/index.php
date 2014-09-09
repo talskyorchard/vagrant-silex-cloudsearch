6868447e5b4bfc219be174dd45e9a36133ee4e5d
Daniel Talsky <talskyorchard@github.com>
2014-09-09 10:49:08 -0400

    Applied PSR-2 formatting to index

--- web/index.php
+++ web/index.php
@@ -1,107 +1,108 @@
 <?php
-//use CloudSearchCall;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require_once __DIR__.'/../vendor/autoload.php'; 
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use CloudSearchCall;
 use Curl\Curl;
 
 $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views',
+    'twig.path' => __DIR__ . '/../views'
 ));
 
 $app['debug'] = true;
-$app['config'] = parse_ini_file(__DIR__.'/../config/config.ini');
+$app['config'] = parse_ini_file(__DIR__ . '/../config/config.ini');
 
 /*
  * PUBLIC PAGES
  */
-$app->get('/', function() use ($app) {
+$app->get('/', function  () use( $app)
+{
     return $app['twig']->render('index.twig', array(
-        'name' => 'test',
+        'name' => 'test'
     ));
 });
 
 /*
  * Single Release Page
  */
-$app->get('/release/{release_id}', function($release_id) use ($app) {
-	return $app['twig']->render('release.twig', array(
-			'release_id' => $release_id,
-	));
-})->assert('release_id', '\d+');
+$app->get('/release/{release_id}', function  ($release_id) use( $app)
+{
+    return $app['twig']->render('release.twig', array(
+        'release_id' => $release_id
+    ));
+})
+    ->assert('release_id', '\d+');
 
 /*
  * Artist Details and Releases for Artist
-*/
-$app->get('/artist/{artist_id}', function($artist_id) use ($app) {
-	return $app['twig']->render('artist.twig', array(
-			'artist_id' => $release_id,
-	));
-})->assert('artist_id', '\d+');
-
-
-
-
+ */
+$app->get('/artist/{artist_id}', function  ($artist_id) use( $app)
+{
+    return $app['twig']->render('artist.twig', array(
+        'artist_id' => $release_id
+    ));
+})
+    ->assert('artist_id', '\d+');
 
 /*
- *  JSON GET API
+ * JSON GET API
  */
 
 /*
  * Basic Search Term
  */
-$app->get('/api/search/{term}', function ($term) use($app) {
-
-	$raw_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/search';
-	$curl = new Curl();
-	$curl->get($raw_url_no_params, array(
-		'q' => $term, 
-		'return' => '_all_fields'));
-	$response = $curl->response;
-
-	//$something = new CloudSearchCall\CloudSearchCallSearch();
-
-	return $app->json($response);
+$app->get('/api/search/{term}', function  ($term) use( $app)
+{
+    
+    $raw_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/search';
+    $curl = new Curl();
+    $curl->get($raw_url_no_params, array(
+        'q' => $term,
+        'return' => '_all_fields'
+    ));
+    $response = $curl->response;
+    
+    // $something = new CloudSearchCall\CloudSearchCallSearch();
+    
+    return $app->json($response);
 });
-	
+
 /*
- * Release 
+ * Release
  */
-$app->get('/api/release/{release_id}', function ($release_id) use($app) {
-
-	$raw_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/search';
-	$curl = new Curl();
-	$curl->get($raw_url_no_params, array(
-		'q' => "(and (term field=release_id '$release_id'))",
-		'q.parser' => 'structured'
-	));
-	$response = $curl->response;
-
-	return $app->json($response);
-	
-})->assert('release_id', '\d+');
+$app->get('/api/release/{release_id}', function  ($release_id) use( $app)
+{
+    
+    $raw_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/search';
+    $curl = new Curl();
+    $curl->get($raw_url_no_params, array(
+        'q' => "(and (term field=release_id '$release_id'))",
+        'q.parser' => 'structured'
+    ));
+    $response = $curl->response;
+    
+    return $app->json($response);
+})
+    ->assert('release_id', '\d+');
 
 /*
  * Artist (List of releases for artist)
-*/
-$app->get('/api/artist/{artist_id}', function ($artist_id) use($app) {
-
-	$raw_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/search';
-	$curl = new Curl();
-	$curl->get($raw_url_no_params, array(
-			'q' => "(and (term field=artist_id '$artist_id'))",
-			'q.parser' => 'structured'
-	));
-	$response = $curl->response;
-
-	return $app->json($response);
-
-})->assert('artist_id', '\d+');
-
-
-
+ */
+$app->get('/api/artist/{artist_id}', function  ($artist_id) use( $app)
+{
+    
+    $raw_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/search';
+    $curl = new Curl();
+    $curl->get($raw_url_no_params, array(
+        'q' => "(and (term field=artist_id '$artist_id'))",
+        'q.parser' => 'structured'
+    ));
+    $response = $curl->response;
+    
+    return $app->json($response);
+})
+    ->assert('artist_id', '\d+');
 
 $app->run();
