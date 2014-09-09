a9fb2cb687ff8169298187d2aba054b0c59cc0d3
Daniel Talsky <talskyorchard@github.com>
2014-09-09 17:17:31 -0400

    Added artist name suggester

--- web/index.php
+++ web/index.php
@@ -5,6 +5,7 @@ ini_set('display_errors', 1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Curl\Curl;
+use Symfony\Component\HttpFoundation\Request;
 
 $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -70,28 +71,34 @@ $app->get('/api/search/{term}', function  ($term) use( $app)
 
 /*
  * Search with facet selection
+ * /api/search/faceted/{term}?facet.genre=Jazz&facet.format=CD
  */
-$app->get('/api/search/{term}/facets/{facet_list}/terms/{terms_list}', function  ($term, $facet_list, $terms_list) use( $app)
+$app->get('/api/search/faceted/{term}', function  (Request $request, $term) use( $app)
 {
-    // faceting logic that should be moved to its own lib
     $facet_whitelist = array('genre');
-    $facets_array = explode(',', $facet_list);
-    $terms_array = explode(',', $terms_list);
-    $facet_container = "(or %s)";
+    $facet_array = false;
+    $get_params = $request->query->all();
     $facet_expressions = '';
-    foreach ($facets_array as $k => $facet_name) {
-        $facet_expressions .= " (and $facet_name:'${$terms_array[$k]}')";
-    }
-    $full_facet_expression = sprintf($facet_container, $facet_expressions);
-    
+    foreach ($get_params as $k => $v) {
+        if (strpos($k, 'facet.') !== FALSE) {
+            list($discard, $facet) = explode('.', $k);
+            if (in_array($facet, $facet_whitelist, TRUE)) {
+                $facet_expressions .= " (and $facet_name:'${$terms_array[$k]}')";
+            }
+        }
+    } 
+    $facet_container = "(or %s)";
+    $full_facet_expression = sprintf($facet_container, $facet_expressions);    
+
     $raw_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/search';
     $curl = new Curl();
-    $curl->get($raw_url_no_params, array(
+    $params_array = array(
         'q' => $term,
         'fq' => $full_facet_expression,
         'facet.genre' => '{}',
         'return' => '_all_fields'
-    ));
+    );
+    $curl->get($raw_url_no_params, $params_array);
     $response = $curl->response;
 
     return $app->json($response);
@@ -102,12 +109,26 @@ $app->get('/api/search/{term}/facets/{facet_list}/terms/{terms_list}', function 
 */
 $app->get('/api/suggest/release_name/{prefix}', function  ($prefix) use($app)
 {
-
     $raw_suggest_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/suggest';
     $curl = new Curl();
     $curl->get($raw_suggest_url_no_params, array(
         'q' => $prefix,
         'suggester' => 'release_name_suggester'
+    ));
+    $response = $curl->response;
+
+    return $app->json($response);
+});
+/*
+ * Artist Name Suggest
+ */
+$app->get('/api/suggest/release_name/{prefix}', function  ($prefix) use($app)
+{
+    $raw_suggest_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/suggest';
+    $curl = new Curl();
+    $curl->get($raw_suggest_url_no_params, array(
+        'q' => $prefix,
+        'suggester' => 'artist_name_suggester'
     ));
     $response = $curl->response;
 
