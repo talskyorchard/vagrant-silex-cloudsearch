214ce5829ba4d508140775d452a508deadca8246
Daniel Talsky <talskyorchard@github.com>
2014-09-09 12:48:42 -0400

    Added basic suggest and faceting

--- web/index.php
+++ web/index.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use CloudSearchCall;
 use Curl\Curl;
 
 $app = new Silex\Application();
@@ -60,12 +60,57 @@ $app->get('/api/search/{term}', function  ($term) use( $app)
     $curl = new Curl();
     $curl->get($raw_url_no_params, array(
         'q' => $term,
+        'facet.genre' => '{}',
         'return' => '_all_fields'
     ));
     $response = $curl->response;
+
+    return $app->json($response);
+});
+
+/*
+ * Search with facet selection
+ */
+$app->get('/api/search/{term}/facets/{facet_list}/terms/{terms_list}', function  ($term, $facet_list, $terms_list) use( $app)
+{
+    // faceting logic that should be moved to its own lib
+    $facet_whitelist = array('genre');
+    $facets_array = explode(',', $facet_list);
+    $terms_array = explode(',', $terms_list);
+    $facet_container = "(or %s)";
+    $facet_expressions = '';
+    foreach ($facets_array as $k => $facet_name) {
+        $facet_expressions .= " (and $facet_name:'${$terms_array[$k]}')";
+    }
+    $full_facet_expression = sprintf($facet_container, $facet_expressions);
     
-    // $something = new CloudSearchCall\CloudSearchCallSearch();
-    
+    $raw_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/search';
+    $curl = new Curl();
+    $curl->get($raw_url_no_params, array(
+        'q' => $term,
+        'fq' => $full_facet_expression,
+        'facet.genre' => '{}',
+        'return' => '_all_fields'
+    ));
+    $response = $curl->response;
+
+    return $app->json($response);
+});
+
+/*
+ * Basic Suggest
+*/
+$app->get('/api/suggest/release_name/{prefix}', function  ($prefix) use($app)
+{
+
+    $raw_suggest_url_no_params = 'http://search-dev-releases-v3-po53rzkarns7qzscfw5tlpk4k4.us-east-1.cloudsearch.amazonaws.com/2013-01-01/suggest';
+    $curl = new Curl();
+    $curl->get($raw_suggest_url_no_params, array(
+        'q' => $prefix,
+        'suggester' => 'release_name_suggester'
+    ));
+    $response = $curl->response;
+
     return $app->json($response);
 });
 
@@ -84,8 +129,8 @@ $app->get('/api/release/{release_id}', function  ($release_id) use( $app)
     $response = $curl->response;
     
     return $app->json($response);
-})
-    ->assert('release_id', '\d+');
+    
+})->assert('release_id', '\d+');
 
 /*
  * Artist (List of releases for artist)
@@ -102,7 +147,7 @@ $app->get('/api/artist/{artist_id}', function  ($artist_id) use( $app)
     $response = $curl->response;
     
     return $app->json($response);
-})
-    ->assert('artist_id', '\d+');
+    
+})->assert('artist_id', '\d+');
 
 $app->run();
